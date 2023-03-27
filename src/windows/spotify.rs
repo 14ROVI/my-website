77c387ec2621c2eff389660ed1aeb5cea99e4ec6
14ROVI <29734170+14ROVI@users.noreply.github.com>
2023-03-27 13:40:08 +0100

    added spotify listen history

--- src/windows/spotify.rs
+++ src/windows/spotify.rs
@@ -1,10 +1,14 @@
+use std::vec;
+
 use yew::prelude::*;
 use gloo::net::websocket::{Message as WsMessage, futures::WebSocket};
 use wasm_bindgen_futures::spawn_local;
 use futures::{channel::mpsc::UnboundedSender, SinkExt, StreamExt};
-use serde_json::Value;
 use gloo::timers::callback::Interval;
 use js_sys::Date;
+use gloo::net::http::Request;
+use serde_json::Value;
+
 
 struct LanyardData {
     album_art: String,
@@ -18,13 +22,16 @@ struct LanyardData {
 #[derive(Debug)]
 pub enum Msg {
     LanyardMessage(WsMessage),
-    UpdateTime
+    UpdateTime,
+    UpdateHistory,
+    SaveHistory(Vec<LastFmHistoryProps>)
 }
 
 pub struct Spotify {
     lanyard_ws_write: UnboundedSender<String>,
     lanyard_data: Option<LanyardData>,
-    update_timer: Option<Interval>
+    update_timer: Option<Interval>,
+    history: Vec<LastFmHistoryProps>,
 }
 impl Component for Spotify {
     type Message = Msg;
@@ -53,10 +60,13 @@ impl Component for Spotify {
             }
         });
 
+        // ctx.link().send_message(Msg::UpdateHistory);
+
         Self {
             lanyard_ws_write: tx,
             lanyard_data: None,
-            update_timer: None
+            update_timer: None,
+            history: vec![]
         }
     }
 
@@ -103,11 +113,47 @@ impl Component for Spotify {
                     }
                 }
 
+                ctx.link().send_message(Msg::UpdateHistory);
+
                 true
             },
             Msg::UpdateTime => {
                 true
             },
+            Msg::UpdateHistory => {
+                let save_history = ctx.link().callback(Msg::SaveHistory);
+
+                spawn_local(async move {
+                    let text = Request::get("https://api.rovi.me/lastfm")
+                        .send()
+                        .await
+                        .unwrap()
+                        .text()
+                        .await
+                        .unwrap();
+                    let json: Value = serde_json::from_str(&text).unwrap();
+                    let tracks = json["recenttracks"]["track"].as_array().unwrap();
+                    let tracks: Vec<LastFmHistoryProps> = tracks.iter().map(|t| {
+                        let t = t.clone();
+                        LastFmHistoryProps {
+                            album_art: t["image"][3]["#text"].as_str().unwrap_or_default().to_string(),
+                            song: t["name"].as_str().unwrap_or_default().to_string(),
+                            artist: t["artist"]["#text"].as_str().unwrap_or_default().to_string(),
+                            listened_at: t["date"]["uts"].as_str().unwrap_or("0").parse().unwrap_or_default(),
+                        }
+                    })
+                    .filter(|p| p.listened_at != 0)
+                    .collect();
+
+                    log::info!("Updated last fm history.");
+                    save_history.emit(tracks);
+                });
+                false
+            },
+            Msg::SaveHistory(history) => {
+                self.history = history;
+                true
+            }
             _ => {
                 log::info!("not covered!");
                 false
@@ -129,13 +175,23 @@ impl Component for Spotify {
             }
 
             html!{
-                <div class="spotify-container">
-                    <img alt="Spotify album art" width="100" height="100" src={ lanyard_data.album_art.clone() }/>
-                    <div>
-                        <p><b>{ lanyard_data.song_name.clone() }</b></p>
-                        <p>{ "On " }{ lanyard_data.album_name.clone() }</p>
-                        <p>{ "By " }{ lanyard_data.artist_name.clone() }</p>
-                        <p id="spotify-song-duration">{ "Elapsed: " }{ format_time(elapsed_time) }{" / "}{ format_time(total_time) }</p>
+                <div class="music-container">
+                    <div class="spotify-container">
+                        <img alt="Spotify album art" width="100" height="100" src={ lanyard_data.album_art.clone() }/>
+                        <div>
+                            <p><b>{ lanyard_data.song_name.clone() }</b></p>
+                            <p>{ "On " }{ lanyard_data.album_name.clone() }</p>
+                            <p>{ "By " }{ lanyard_data.artist_name.clone() }</p>
+                            <p id="spotify-song-duration">{ "Elapsed: " }{ format_time(elapsed_time) }{" / "}{ format_time(total_time) }</p>
+                        </div>
+                    </div>
+                    <div class="lastfm-scroll-container">
+                        <div class="lastfm-container">
+                            { self.history.iter().map(|p|
+                                html! { <LastFmHistory ..p.clone()/> }
+                                ).collect::<Html>()
+                            }
+                        </div>
                     </div>
                 </div>
             }
@@ -144,5 +200,69 @@ impl Component for Spotify {
                 <p>{ "Not currently listening to anything :(" }</p>
             }
         }
+    }
+}
+
+
+#[derive(Properties, PartialEq, Debug, Clone)]
+pub struct LastFmHistoryProps {
+    pub album_art: String,
+    pub song: String,
+    pub artist: String,
+    pub listened_at: u64,
+}
+
+#[function_component(LastFmHistory)]
+pub fn last_fm_history(props: &LastFmHistoryProps) -> Html {
+    let current_time = Date::now() as u64 / 1000;
+    let elapsed = current_time - props.listened_at;
+
+    let s_per_minute = 60;
+    let s_per_hour = 3600;
+    let s_per_day = 86400;
+    let s_per_month = 2592000;
+    let s_per_year = 31536000;
+
+    let minutes_passed = elapsed / s_per_minute;
+    let hours_passed = elapsed / s_per_hour;
+    let days_passed = elapsed / s_per_day;
+    let months_passed = elapsed / s_per_month;
+    let years_passed = elapsed / s_per_year;
+
+    let formatted_time = if years_passed > 1 {
+        format!("{} years ago", years_passed)
+    } else if years_passed == 1 {
+        String::from("1 year ago")
+    } else if months_passed > 1 {
+        format!("{} months ago", months_passed)
+    } else if months_passed == 1 {
+        String::from("1 month ago")
+    } else if days_passed > 1 {
+        format!("{} days ago", days_passed)
+    } else if days_passed == 1 {
+        String::from("1 day ago")
+    } else if hours_passed > 1 {
+        format!("{} hours ago", hours_passed)
+    } else if hours_passed == 1 {
+        String::from("1 hour ago")
+    } else if minutes_passed > 1 {
+        format!("{} minutes ago", minutes_passed)
+    } else if minutes_passed == 1 {
+        String::from("1 minute ago")
+    } else if elapsed > 1 {
+        format!("{} seconds ago", elapsed)
+    } else {
+        String::from("1 second ago")
+    };
+
+    html!{
+        <div>
+            <img alt="Track album art" width="50" height="50" src={ props.album_art.clone() }/>
+            <div>
+                <p><b>{ props.song.clone() }</b></p>
+                <p>{ props.artist.clone() }</p>
+                <p>{ formatted_time }</p>
+            </div>
+        </div>
     }
 }
